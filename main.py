f69b59a0ba02baa26012cccd3822a5dfdb403544
Kadah-Ne <martin.gouviaux@epsi.fr>
2022-11-18 09:09:42 +0100

    repaai

--- main.py
+++ main.py
@@ -14,5 +14,8 @@ def index():
          return render_template('index.html')
       else :
          return render_template('login.html')
+   else :
+      return render_template('login.html')
+
 if __name__ == '__main__':
    app.run()
