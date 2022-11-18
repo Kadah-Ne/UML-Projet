1446eee30569f3b4b0fe49e53da65c49f4b8e0a1
remy385 <remy.gaboriau@gmail.com>
2022-11-18 13:34:13 +0100

    database

--- templates/test.py
+++ templates/test.py
@@ -1,4 +1,4 @@
-import DBmanager
+import sqlite3
 
 conn = sqlite3.connect('ma_base.db')
 
@@ -30,25 +30,6 @@ elif format != "None":
     cursor.execute("""SELECT barcode,tag FROM BookItem where format=? and library=?""", (format,lib,))
 
 rows = cursor.fetchall()
-
-with DBmanager.open('cache', 'c') as db:
-
-    # Record some values
-    db[b'hello'] = b'there'
-    db['www.python.org'] = 'Python Website'
-    db['www.cnn.com'] = 'Cable News Network'
-
-    # Note that the keys are considered bytes now.
-    assert db[b'www.python.org'] == b'Python Website'
-    # Notice how the value is now in bytes.
-    assert db['www.cnn.com'] == b'Cable News Network'
-
-    # Often-used methods of the dict interface work too.
-    print(db.get('python.org', b'not present'))
-
-    # Storing a non-string key or value will raise an exception (most
-    # likely a TypeError).
-    db['www.yahoo.com'] = 4
 
 for row in rows:
     print('{0} , {1}'.format(row[0], row[1]))
