da8073b0603f170ecb4d1a3d22b859573296b1bb
Kadah-Ne <martin.gouviaux@epsi.fr>
2022-11-18 13:55:12 +0100

    Search done

--- main.py
+++ main.py
@@ -13,22 +13,29 @@ def index():
       if request.form['idTxt'] == "Martin" and request.form['pwdTxt'] == '1':
          session["idTxt"] = request.form['idTxt']
          session["isLib"] = True
-         return catalog()
+         return redirect("/search")
       else :
          return render_template('login.html')
    else :
       return render_template('login.html')
 
-@app.route('/catalog', methods=['GET','POST'])
-def catalog():
-   data = tuple(DBmanager.characterlist()) # format in ((info))
-   heading = ("ISBN", "name", "overview", "publisher", "publicationDate", "lang")
-   if request.method == "POST":
-      return render_template("index.html", headings=heading, data=data,user = session.get("idTxt"),library = session.get("Library")) #render_template te permet d'executer un HTML(doit etre dans dossier template)
 
 @app.route("/search",methods = ["GET","POST"])
 def search():
-   return render_template("search.html")
+   data = tuple(DBmanager.characterlist()) # format in ((info))
+   heading = ("ISBN", "name", "overview", "publisher", "publicationDate", "lang")
+   print(session.get("idTxt"))
+   if request.method == "POST":
+      bookTitle = request.form["searchTitle"]
+      bookAuthor = request.form["searchAuthor"]
+      bookTopic = request.form["searchTopic"]
+      bookFormat = request.form["format"]
+      if bookAuthor != "" or bookTitle != "" or bookTopic != "" or bookFormat !="":
+         return render_template("search.html",user = session.get("idTxt"), data = data, headings = heading, Title = bookTitle, Author = bookAuthor, Topic = bookTopic, Format = bookFormat) #render_template te permet d'executer un HTML(doit etre dans dossier template)
+      else :
+         return render_template("search.html",user = session.get("idTxt"), data = data, headings = heading)
+   else :
+      return render_template("search.html",user = session.get("idTxt"), data = data, headings = heading)
 
 if __name__ == '__main__':
    app.secret_key ="2ifnidkohéijfhizdhnazfnaz,faznfç(jicno)"
